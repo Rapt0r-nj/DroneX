e4f8c63776e23b5b708f8971fd460c4b066f3741
Kabir <mahiulkabir.cs@gmail.com>
2024-06-14 01:40:30 +0600

    commit 2

--- README.md
+++ README.md
@@ -2,18 +2,18 @@
 Drone x Flight Controller Simulator
 
 ### objective
-***Learn ROS***
-
-To Simulate a drone, And Implement its flight controller with PID control build from scratch.
+***Learn ROS, Project Architecture and good Coding Habits***
 
 ### tasks
+  ***simulate a drone, then move it from some random initial position and rotation to hovering state via PID control.***  
 - **Phase 0**
-  - Set up drones attributes: position, orientation, rotor speeds. 
-  - Simulate the drones movement based on atributes.
-  - Add controls.
+  - Set up the drone in C++: position, orientation, rotor speeds. 
+  - Simulate the drones movement based on attributes.
 - **Phase 1**
   - Set up basic PID control.
   - Distribute the PID control over 4 rotors.
-  - Sync everything to make the flight controller.
 - **Phase 2**
+  - Sync everything to make the flight controller.
   - Shift the project to ROS.
+- **Bonus**
+  - Implement AI and train it to tune the PID control to achieve the target state AFAP.
--- drone.h
+++ drone.h
@@ -1,4 +1,8 @@
 
+#define FREQ 1 
+#define MASS 1
+#define GACC 9.8
+
 struct vec3{
   double x = 0.0;
   double y = 0.0;
@@ -7,10 +11,23 @@ struct vec3{
   vec3(double x = 0, double y = 0, double z = 0) : x(x), y(y), z(z) {}
 
   void print(){
-  std::cout<<"Vec3("<<x<<", "<<y<<", "<<z<<")\n";
+  std::cout << "Vec3(" << x << ", " << y << ", " << z << ")\n";
   }
   vec3 operator+(const vec3& other){
-    return vec3(x+other.x, y+other.y, z+other.z);
+    return vec3(x + other.x, y + other.y, z + other.z);
+  }
+  vec3 operator*(const double& other){
+    return vec3(x * other, y * other.y, z * other.z);
+  }
+  double dot(const vec3& other){
+    return x * other.x + y * other.y + z * other.z
+  }
+  vec3 cross(const vec3$ other){
+    return vec3(
+      y * other.z - z * other.y,
+      z * other.x - x * other.z,
+      x * other.y - y * other.x
+    )
   }
 };
 
@@ -22,9 +39,10 @@ struct drone{
   vec3 vel;
   vec3 avel;
 
-  //rotor thrust
-  double r1 = 0; //front right 
-  double r2 = 0; //front left
-  double r3 = 0; //back left
-  double r4 = 0; //back right
+  //rotor thrust, [0, 1]
+  double r1 = 0.0; //front right 
+  double r2 = 0.0; //front left
+  double r3 = 0.0; //back left
+  double r4 = 0.0; //back right
+
 };
