94e3f0bb681fa343a48a818b14d3d8e11ce348e0
Kabir <mahiulkabir.cs@gmail.com>
2024-06-14 00:47:24 +0600

    commit 1

--- drone.h
+++ drone.h
@@ -3,6 +3,15 @@ struct vec3{
   double x = 0.0;
   double y = 0.0;
   double z = 0.0;
+
+  vec3(double x = 0, double y = 0, double z = 0) : x(x), y(y), z(z) {}
+
+  void print(){
+  std::cout<<"Vec3("<<x<<", "<<y<<", "<<z<<")\n";
+  }
+  vec3 operator+(const vec3& other){
+    return vec3(x+other.x, y+other.y, z+other.z);
+  }
 };
 
 struct drone{
@@ -12,9 +21,6 @@ struct drone{
 
   vec3 vel;
   vec3 avel;
-
-  vec3 acc;
-  vec3 aacc;
 
   //rotor thrust
   double r1 = 0; //front right 
--- main.cpp
+++ main.cpp
@@ -6,9 +6,11 @@
 #define FREQ 1 
 
 int main(){
-  drone x;
+  drone Drone;
+  vec3 add(0, 0, 1);
   while(1){
-    std::cout << "gameloop\n"; 
+    Drone.pos = Drone.pos + add;
+    Drone.pos.print();
     std::this_thread::sleep_for(std::chrono::seconds(1/FREQ));
   }
 }
